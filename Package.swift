0a2f5f6845c6109f12dabe4088fa38e0e638a385
Ryan Krol <ryankr@hotmail.co.uk>
2018-12-20 17:22:39 +0000

    Migrate result and errors to SwiftToolbox

--- Package.swift
+++ Package.swift
@@ -6,15 +6,14 @@ import PackageDescription
 let package = Package(
     name: "SneakersNotificationCenter",
     dependencies: [
-        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "1.7.4"),
-        .package(url: "https://github.com/IBM-Swift/Swift-SMTP.git", from: "5.1.0"),
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", .upToNextMajor(from: "1.7.4")),
+        .package(url: "https://github.com/IBM-Swift/Swift-SMTP.git", .upToNextMajor(from: "5.1.0")),
+        .package(url: "https://github.com/RyanMKrol/SwiftToolbox.git", .branch("master")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SneakersNotificationCenterLib",
-            dependencies: ["SwiftSoup", "SwiftSMTP"]),
+            dependencies: ["SwiftSoup", "SwiftSMTP", "SwiftToolbox"]),
         .target(
             name: "SneakersNotificationCenter",
             dependencies: ["SneakersNotificationCenterLib"]),
--- Sources/SneakersNotificationCenterLib/DataCollection/PageScraper.swift
+++ Sources/SneakersNotificationCenterLib/DataCollection/PageScraper.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import SwiftSoup
+import ToolLibrary
 
 public class PageScraper {
 
